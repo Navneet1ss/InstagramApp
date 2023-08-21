b12c13fb6968f48aced3d28fc5f1cd836b89c0c3
Navneet Bhargava <72698663+Navneet1ss@users.noreply.github.com>
2023-08-21 12:05:08 +1100

    Add devise and assosiation with pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,7 @@
 class PicsController < ApplicationController
    before_action :find_pic, only:[:show, :edit, :update, :destroy]
   def index
-    @pics = Pic.all
+    @pics = Pic.all.order("created_at DESC")
   end
   
   def show
@@ -9,11 +9,13 @@ class PicsController < ApplicationController
   end
 
   def new
-    @pic = Pic.new
+    #@pic = Pic.new
+    @pic = current_user.pics.build
   end
 
   def create
-    @pic =Pic.new(pic_params)
+    #@pic =Pic.new(pic_params)
+    @pic= current_user.pics.build(pic_params)
 
     if @pic.save
         redirect_to @pic, notice:"yes it was posted"
